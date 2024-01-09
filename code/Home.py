07c153c1300445d52e34482c21283911240b37f8
Daniele Vitale <vitaled@gmail.com>
2024-01-09 14:30:48 +0100

    feat: check for user in homepage

--- code/Home.py
+++ code/Home.py
@@ -7,33 +7,17 @@ import traceback
 from utilities.LLMHelper import LLMHelper
 import logging
 from utilities.StreamlitHelper import StreamlitHelper
+from utilities.SessionHelper import SessionHelper
 
 logger = logging.getLogger('azure.core.pipeline.policies.http_logging_policy').setLevel(logging.WARNING)
-
 try:
-   # Optional -- adds the title and icon to the current page
-    #add_page_title()
-
-    # Specify what pages should be shown in the sidebar, and what their titles and icons
-    # should be
-    # show_pages(
-    #     [
-    #         Page("Home.py", "Home"),
-    #         Page("pages/01_Aggiunta_CV.py", "Aggiungi CV Candidato"),
-    #         Page("pages/02_Analisi_CV.py", "Analisi Candidato",),
-    #         Page("pages/03_Prompt.py", "Gestione Profili"),    
-    #         Page("pages/04_QA_CV.py", "Q/A Candidato", ),
-    #         Page("pages/05_Admin.py", "Impostazioni")
-    #     ]
-    # )
-
+    
     st.set_page_config(layout="wide")
     StreamlitHelper.hide_footer()
-
     st.title("HR Assistant Open AI")
-    
-    llm_helper = LLMHelper()
-    st.image(os.path.join('images','citta-metropolitana-roma-capitale-logo.png'))
-    
+    st.image(os.path.join('images','citta-metropolitana-roma-capitale-logo.png'))   
+    logger.info('Connecting to Home Page')
+    user = SessionHelper.get_current_user()
+    logger.info('Connected User: ' + user.name)
 except Exception:
     st.error(traceback.format_exc())
