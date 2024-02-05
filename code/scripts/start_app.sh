82080882fdaafb595fc827292825e5191592c634
Daniele Vitale <vitaled@gmail.com>
2024-02-05 11:33:28 +0100

    fix: wrong path

--- code/scripts/start_app.sh
+++ code/scripts/start_app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python scripts/initCosmosDb.py;
-streamlit run Home.py --server.port 8080 --server.enableXsrfProtection false;
+python ./scripts/initCosmosDb.py;
+streamlit run Home.py --server.port 8080 --server.enableXsrfProtection false --server.enableCORS=false;
