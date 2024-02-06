fe7e3f21bd2694f231693982263e946a5a2b3891
Daniele Vitale <vitaled@gmail.com>
2024-02-06 12:25:39 +0100

    add max_item_count

--- code/utilities/AzureCosmosDBClient.py
+++ code/utilities/AzureCosmosDBClient.py
@@ -34,7 +34,7 @@ class AzureCosmosDBClient:
     def delete_resumes(self):
         query = "SELECT * FROM resumes r"
         items = self.resumes.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         for item in items:
             self.resumes.delete_item(item['id'], item['id'])
         # Get all the elements and delete them one by one
@@ -49,7 +49,7 @@ class AzureCosmosDBClient:
     def delete_candidates(self):
         query = "SELECT * FROM candidates c"
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         for item in items:
             self.candidates.delete_item(item['id'], item['id'])
 
@@ -61,8 +61,8 @@ class AzureCosmosDBClient:
     def delete_analyses(self):
         query = "SELECT * FROM analyses a"
         items = self.analyses.query_items(
-            query=query, enable_cross_partition_query=True)
-        
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
+
         for item in items:
             self.analyses.delete_item(item['id'], item['id'])
 
@@ -74,7 +74,7 @@ class AzureCosmosDBClient:
     def delete_upload_runs(self):
         query = "SELECT * FROM uploadruns u"
         items = self.upload_runs.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         for item in items:
             self.upload_runs.delete_item(item['id'], item['id'])
 
@@ -86,7 +86,7 @@ class AzureCosmosDBClient:
     def delete_analyses_runs(self):
         query = "SELECT * FROM analysesruns a"
         items = self.analyses_runs.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         for item in items:
             self.analyses_runs.delete_item(item['id'], item['id'])
 
@@ -113,19 +113,19 @@ class AzureCosmosDBClient:
     def get_analysis_by_candidate_id(self, candidate_id):
         query = f"SELECT * FROM analyses a WHERE a.CandidateId = '{candidate_id}'"
         items = self.analyses.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_analysis_by_candidate_id_and_profile(self, candidate_id, profile_id):
         query = f"SELECT * FROM analyses a WHERE a.CandidateId = '{candidate_id}' AND a.ProfileId = '{profile_id}' OFFSET 0 LIMIT 1000"
         items = self.analyses.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def delete_analysis_by_profile(self, profile_id):
         query = f"SELECT * FROM analyses a WHERE a.ProfileId = '{profile_id}'"
         items = self.analyses.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         for item in items:
             self.analyses.delete_item(item['id'], item['id'])
 
@@ -141,46 +141,46 @@ class AzureCosmosDBClient:
     def get_candidates(self, offset=0, limit=1000):
         query = f"SELECT * FROM candidates c OFFSET {offset} LIMIT {limit}"
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_candidates_count(self):
         query = "SELECT VALUE COUNT(1) FROM candidates c"
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         count = list(items)[0]
         return count
 
     def get_candidate_by_id(self, id):
         query = f"SELECT * FROM candidates c WHERE c.CandidateId = '{id}'"
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_candidates_by_resume_id(self, resume_id):
         query = f"SELECT * FROM candidates c WHERE c.resume_id = '{resume_id}'"
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_candidates_with_candidacy_count(self):
         query = "SELECT VALUE COUNT(1) FROM candidates c WHERE ARRAY_LENGTH(c.candidature) > 0"
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         count = list(items)[0]
         return count
 
     def get_candidates_with_evaluation_count(self):
         query = "SELECT VALUE COUNT(1) FROM candidates c WHERE IS_DEFINED(c.Valutazioni)"
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         count = list(items)[0]
         return count
 
     def get_candidates_with_history_count(self):
         query = 'SELECT VALUE COUNT(1) FROM candidates c WHERE ARRAY_LENGTH(c["Storia Rapporto Lavorativo"]) > 0'
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         count = list(items)[0]
         return count
 
@@ -188,13 +188,13 @@ class AzureCosmosDBClient:
         cf = cf.upper()
         query = f"SELECT * FROM candidates c WHERE UPPER(c.CodiceFiscale) = '{cf}'"
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_candidate_by_profile(self, profile):
         query = f"SELECT * FROM candidates c WHERE ARRAY_CONTAINS(c.candidature, '{profile}')"
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_candidate_by_profiles(self, profiles: List[str]):
@@ -205,7 +205,7 @@ class AzureCosmosDBClient:
         for profile in profiles[1:]:
             query += f" OR ARRAY_CONTAINS(c.candidature, '{profile}')"
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_candidate_by_name_and_surname(self, name, surname):
@@ -213,19 +213,19 @@ class AzureCosmosDBClient:
         surname = surname.lower()
         query = f"SELECT * FROM candidates c WHERE LOWER(c.Nome) = '{name}' AND LOWER(c.Cognome) = '{surname}'"
         items = self.candidates.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_resumes(self):
         query = "SELECT * FROM resumes r"
         items = self.resumes.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_resume_by_id(self, id):
         query = f"SELECT * FROM resumes r WHERE r.id = '{id}'"
         items = self.resumes.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def put_resume(self, resume):
@@ -234,19 +234,19 @@ class AzureCosmosDBClient:
     def get_users(self):
         query = "SELECT * FROM users u"
         items = self.users.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_user_by_id(self, id):
         query = f"SELECT * FROM users u WHERE u.id = '{id}'"
         items = self.users.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_user(self, principal_id):
         query = f"SELECT * FROM users u WHERE u.id = '{principal_id}'"
         items = self.users.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def put_user(self, principal_id, principal_name, role, profiles):
@@ -261,19 +261,19 @@ class AzureCosmosDBClient:
     def get_profiles(self):
         query = "SELECT * FROM profiles p"
         items = self.profiles.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_profile_by_id(self, id):
         query = f"SELECT * FROM profiles p WHERE p.profile_id = '{id}'"
         items = self.profiles.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_profile_by_description(self, profile_description):
         query = f"SELECT * FROM profiles p WHERE p.profile_description = '{profile_description}'"
         items = self.profiles.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def update_profile(self, profile):
@@ -293,19 +293,19 @@ class AzureCosmosDBClient:
     def get_batch_upload_run_ids(self):
         query = f"SELECT distinct c.run_id FROM c"
         items = self.upload_runs.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_upload_run(self, id):
         query = f"SELECT * FROM uploadruns u WHERE u.id = '{id}'"
         items = self.upload_runs.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_upload_runs(self, run_id):
         query = f"SELECT * FROM uploadruns u WHERE u.run_id = '{run_id}'"
         items = self.upload_runs.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def set_upload_run_completed(self, id):
@@ -323,7 +323,7 @@ class AzureCosmosDBClient:
     def set_upload_run_status(self, id, status):
         query = f"SELECT * FROM uploadruns u WHERE u.id = '{id}'"
         items = self.upload_runs.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         upload_run = list(items)[0]
         upload_run['status'] = status
         self.upload_runs.upsert_item(upload_run)
@@ -331,7 +331,7 @@ class AzureCosmosDBClient:
     def set_upload_run_error_message(self, id, error_message):
         query = f"SELECT * FROM uploadruns u WHERE u.id = '{id}'"
         items = self.upload_runs.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         upload_run = list(items)[0]
         upload_run['error_message'] = error_message
         self.upload_runs.upsert_item(upload_run)
@@ -339,7 +339,7 @@ class AzureCosmosDBClient:
     def set_upload_run_ending_date(self, id):
         query = f"SELECT * FROM uploadruns u WHERE u.id = '{id}'"
         items = self.upload_runs.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         upload_run = list(items)[0]
         upload_run['ending_time'] = datetime.utcnow().strftime(
             "%Y-%m-%d %H:%M:%S")
@@ -348,14 +348,14 @@ class AzureCosmosDBClient:
     def get_upload_runs_by_status(self, status):
         query = f"SELECT * FROM uploadruns u WHERE u.status = '{status}'"
         items = self.upload_runs.query_items(
-            query=query, enable_cross_partition_query=True)
+            query=query, enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def get_analyses_runs_by_status(self, status):
         query = f"SELECT * FROM analysesruns a WHERE a.status = '{status}'"
         items = self.analyses_runs.query_items(
             query=query,
-            enable_cross_partition_query=True)
+            enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     # Analyses Runs
@@ -384,7 +384,7 @@ class AzureCosmosDBClient:
         query = f"SELECT * FROM analysesruns a WHERE a.run_id = '{run_id}'"
         items = self.analyses_runs.query_items(
             query=query,
-            enable_cross_partition_query=True)
+            enable_cross_partition_query=True, max_item_count=1000)
         return items
 
     def set_analysis_run_completed(self, id):
@@ -403,7 +403,7 @@ class AzureCosmosDBClient:
         query = f"SELECT * FROM analysesruns a WHERE a.id = '{id}'"
         items = self.analyses_runs.query_items(
             query=query,
-            enable_cross_partition_query=True)
+            enable_cross_partition_query=True, max_item_count=1000)
         run = list(items)[0]
         run['status'] = status
         self.analyses_runs.upsert_item(run)
@@ -412,7 +412,7 @@ class AzureCosmosDBClient:
         query = f"SELECT * FROM analysesruns a WHERE a.id = '{id}'"
         items = self.analyses_runs.query_items(
             query=query,
-            enable_cross_partition_query=True)
+            enable_cross_partition_query=True, max_item_count=1000)
         run = list(items)[0]
         run['error_message'] = error_message
         self.analyses_runs.upsert_item(run)
@@ -431,5 +431,5 @@ class AzureCosmosDBClient:
         query = f"SELECT * FROM analysesruns a WHERE a.status = '{status}'"
         items = self.analyses_runs.query_items(
             query=query,
-            enable_cross_partition_query=True)
+            enable_cross_partition_query=True, max_item_count=1000)
         return items
